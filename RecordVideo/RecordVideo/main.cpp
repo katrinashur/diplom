408d69131d5e1fa48ff75314847d40c03bf9ff3e
Екатерина Шурлаева <katrinashur@gmail.com>
2020-05-20 22:47:21 +0400

    time in frames names

--- RecordVideo/RecordVideo/main.cpp
+++ RecordVideo/RecordVideo/main.cpp
@@ -1,10 +1,12 @@
 #include "opencv2/opencv.hpp"
+#include <windows.h>
+#include <stdio.h>
 using namespace std;
 using namespace cv;
 
 int main(int, char**)
 {
-	VideoCapture cap(1); // open the default camera
+	VideoCapture cap(0); // open the default camera
 	if (!cap.isOpened())  // check if we succeeded
 	{
 		cout << "ERROR: There is no web-camera to connect!" << endl;
@@ -16,10 +18,13 @@ int main(int, char**)
 	cap.set(CAP_PROP_FRAME_HEIGHT, 960);
 
 	Mat frame;
-	int i = 1;
 	while (true) {
+		SYSTEMTIME st;	
 		cap >> frame; //-- захватываем очередной кадр
-		string filename =to_string(i) + ".jpeg";
+		GetLocalTime(&st);
+
+		string filename = to_string(st.wYear) +"-"+ to_string(st.wMonth)+ "-" + to_string(st.wDay) + " " +
+			to_string(st.wHour) + "_"+ to_string(st.wMinute)+"_"+ to_string(st.wSecond) + "_" + to_string(st.wMilliseconds) + ".jpeg";
 		imwrite(filename, frame); //--cохраняем в файл
 
 		char c = waitKey(33); //-- если была нажата клавиша, узнаём её код
@@ -27,7 +32,6 @@ int main(int, char**)
 		if (c == 27) { //-- нажата ESC, прерываем цикл
 			break;
 		}
-		i++;
 	}
 
 	
