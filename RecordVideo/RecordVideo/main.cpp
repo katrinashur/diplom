b7b9171a51f66728a1648721fc2a4779b1dcc691
Екатерина Шурлаева <katrinashur@gmail.com>
2020-05-25 14:49:14 +0400

    cooperation of processes

--- RecordVideo/RecordVideo/main.cpp
+++ RecordVideo/RecordVideo/main.cpp
@@ -1,40 +1,111 @@
 #include "opencv2/opencv.hpp"
 #include <windows.h>
 #include <stdio.h>
+#include <fstream>
 using namespace std;
 using namespace cv;
 
+
+
 int main(int, char**)
 {
+	HANDLE   hNamedPipe;
+	char     pchMessage[1] = {};   // для сообщения
+	int    nMessageLength;   // длина сообщения
+	char  pchResponse[80];
+	int responseCode = 0;
+
+	SYSTEMTIME st;
 	VideoCapture cap(0); // open the default camera
+
 	if (!cap.isOpened())  // check if we succeeded
 	{
 		cout << "ERROR: There is no web-camera to connect!" << endl;
 		return -1;
 	}
-		
+
 	//-- Выставляем параметры камеры ширину и высоту кадра в пикселях
 	cap.set(CAP_PROP_FRAME_WIDTH, 1280);
 	cap.set(CAP_PROP_FRAME_HEIGHT, 960);
 
 	Mat frame;
-	while (true) {
-		SYSTEMTIME st;	
+
+	// создаем именованный канал для чтения и записи
+	hNamedPipe = CreateNamedPipe(
+		"\\\\.\\pipe\\demo_pipe",  // имя канала
+		PIPE_ACCESS_INBOUND,        // читаем из канала 
+		PIPE_TYPE_MESSAGE | PIPE_WAIT,   // синхронная передача сообщений
+		1,         // максимальное количество экземпляров канала 
+		0,         // размер выходного буфера по умолчанию
+		0,         // размер входного буфера по умолчанию
+		INFINITE,  // клиент ждет связь бесконечно долго
+		NULL       // безопасность по умолчанию
+	);
+	// проверяем на успешное создание
+	if (hNamedPipe == INVALID_HANDLE_VALUE)
+	{
+		cerr << "Create named pipe failed." << endl
+			<< "The last error code: " << GetLastError() << endl;
+		cout << "Press any key to exit.";
+		cin.get();
+
+		return 0;
+	}
+
+	// ждем, пока клиент свяжется с каналом
+	cout << "The server is waiting for connection with a client." << endl;
+	if (!ConnectNamedPipe(
+		hNamedPipe,    // дескриптор канала
+		NULL      // связь синхронная
+	))
+	{
+		cerr << "Connect named pipe failed." << endl
+			<< "The last error code: " << GetLastError() << endl;
+		CloseHandle(hNamedPipe);
+		cout << "Press any key to exit.";
+		cin.get();
+
+		return 0;
+	}
+
+
+	DWORD   dwBytesRead = 0;     // для количества прочитанных байтов
+	DWORD   dwBytesAvail = 0;    // для количества доступных байтов
+	DWORD   dwBytesLeft = 0;    // для количества оставшихся байтов
+
+	while (true)
+	{
+		if (!PeekNamedPipe(
+			hNamedPipe,
+			pchMessage,
+			sizeof(pchMessage),
+			&dwBytesRead,
+			&dwBytesAvail,
+			&dwBytesLeft)
+			&& pchMessage[0] != 0)
+		{
+			cerr << "Data reading from the named pipe failed." << endl
+			<< "The last error code: " << GetLastError() << endl;
+			CloseHandle(hNamedPipe);
+		}
+		else if(pchMessage[0] != '\0') //команда закончить работу
+		{
+			// выводим полученное от клиента сообщение на консоль
+			cout << "The server received the message from a client: " << endl;
+			cout << pchMessage[0] << endl;
+			break;
+		}
+
 		cap >> frame; //-- захватываем очередной кадр
 		GetLocalTime(&st);
 
-		string filename = to_string(st.wYear) +"-"+ to_string(st.wMonth)+ "-" + to_string(st.wDay) + " " +
-			to_string(st.wHour) + "_"+ to_string(st.wMinute)+"_"+ to_string(st.wSecond) + "_" + to_string(st.wMilliseconds) + ".jpeg";
+		string filename = to_string(st.wYear) + "-" + to_string(st.wMonth) + "-" + to_string(st.wDay) + " " +
+			to_string(st.wHour) + "_" + to_string(st.wMinute) + "_" + to_string(st.wSecond) + "_" + to_string(st.wMilliseconds) + ".jpeg";
 		imwrite(filename, frame); //--cохраняем в файл
-
-		char c = waitKey(33); //-- если была нажата клавиша, узнаём её код
-
-		if (c == 27) { //-- нажата ESC, прерываем цикл
-			break;
-		}
 	}
 
-	
-	// the camera will be deinitialized automatically in VideoCapture destructor
+	// закрываем дескриптор канала 
+	CloseHandle(hNamedPipe);
+	// завершаем процесс
 	return 0;
 }
--- gui_test/venv/main.py
+++ gui_test/venv/main.py
@@ -1,24 +1,35 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QAction, QFileDialog, QApplication)
+from PyQt5.QtGui import QIcon
 import form  # Это наш конвертированный файл дизайна
 import subprocess
+import win32pipe, win32file, pywintypes
+import time
+
 
 class ExampleApp(QtWidgets.QMainWindow, form.Ui_MainWindow):
     def __init__(self):
-        # Это здесь нужно для доступа к переменным, методам
-        # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-		self.start.clicked.connect(self.startExperiment)  # Выполнить функцию browse_folder
-        # при нажатии кнопки
+        self.start.clicked.connect(self.start_experiment)
+        self.stop.clicked.connect(self.stop_experiment)
 
-    def startExperiment(self):
-        cmd = 'RecordVideo.exe'
-        import subprocess
-        PIPE = subprocess.PIPE
-        p = subprocess.Popen(cmd, shell=True)   #запускаем запись видео
-        p.wait()
-        print("process finished")
+        self.videoRecorder = VideoRecorder('')
+        self.openDirectory.clicked.connect(self.open_folder)
+        self.dir = ''
+
+    def open_folder(self):
+        folder = str(QFileDialog.getExistingDirectory(self, "Open folder"))
+        self.experiment_path.setText(folder)
+
+    def start_experiment(self):
+        experiment_path = self.experimentPath.text()
+        self.videoRecorder.start_record()
+        print(experiment_path)
+
+    def stop_experiment(self):
+        self.videoRecorder.stop_record()
 
 
 def main():
@@ -27,6 +38,34 @@ def main():
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
+
+class VideoRecorder():
+    def __init__(self, experiment_path):
+        self.experiment_path = experiment_path
+        self.proc = None
+        self.handle = None
+
+    def start_record(self):
+        cmd = 'RecordVideo.exe ' #+ self.experiment_path
+        import subprocess
+        PIPE = subprocess.PIPE
+        self.proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE)  # запускаем запись видео
+        time.sleep(3)
+        self.handle = win32file.CreateFile(
+            r'\\.\pipe\demo_pipe',
+            win32file.GENERIC_WRITE,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None)
+
+    def stop_record(self):
+        test_data = "0".encode("ascii")
+        win32file.WriteFile(self.handle, test_data)  # команда закончить работу!
+
+
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     main()  # то запускаем функцию main()
+
 
