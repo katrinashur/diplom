d870f46390efd599709e80ab0686821fc75d2f6e
Екатерина Шурлаева <katrinashur@gmail.com>
2020-05-25 14:47:46 +0400

    add stop button

--- gui_test/venv/form.py
+++ gui_test/venv/form.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\Documents\DIPLOM\gui_test\form.ui'
+# Form implementation generated from reading ui file 'form.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
@@ -13,23 +13,46 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(304, 161)
+        MainWindow.resize(373, 219)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.experimentName = QtWidgets.QLineEdit(self.centralwidget)
+        self.experimentName.setObjectName("experimentName")
+        self.verticalLayout_2.addWidget(self.experimentName)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.experimentPath = QtWidgets.QLineEdit(self.centralwidget)
+        self.experimentPath.setObjectName("experimentPath")
+        self.horizontalLayout_2.addWidget(self.experimentPath)
+        self.openDirectory = QtWidgets.QPushButton(self.centralwidget)
+        self.openDirectory.setObjectName("openDirectory")
+        self.horizontalLayout_2.addWidget(self.openDirectory)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout.addLayout(self.verticalLayout_2)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.stop = QtWidgets.QPushButton(self.centralwidget)
-        self.stop.setObjectName("stop")
-        self.horizontalLayout.addWidget(self.stop)
         self.start = QtWidgets.QPushButton(self.centralwidget)
         self.start.setObjectName("start")
         self.horizontalLayout.addWidget(self.start)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.stop = QtWidgets.QPushButton(self.centralwidget)
+        self.stop.setObjectName("stop")
+        self.horizontalLayout.addWidget(self.stop)
         self.verticalLayout.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 304, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 373, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -42,5 +65,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.stop.setText(_translate("MainWindow", "stop"))
-        self.start.setText(_translate("MainWindow", "start"))
+        self.label.setText(_translate("MainWindow", "Введите название эксперимента"))
+        self.label_2.setText(_translate("MainWindow", "Выберите папку, куда сохранить результаты эксперимента"))
+        self.openDirectory.setText(_translate("MainWindow", "Открыть"))
+        self.start.setText(_translate("MainWindow", "Начать эксперимент"))
+        self.stop.setText(_translate("MainWindow", "Закончить эксперимент"))
